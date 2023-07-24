290064d6b603ea4dae4acfb6a05ba7337cc8cc0c
Hamza_OK <hamza.okhadir2018@gmail.com>
2023-07-24 18:51:17 +0100

    update the logo section

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,23 +1,34 @@
 import React from "react";
 import Link from "next/link";
 import {motion} from "framer-motion";
+import { Logo_Light, Logo_Dark } from "./Icons";
 
 const MotionLink = motion(Link);
 export const Logo = () => {
   return (
-    <div className="items-center flex justify-center ">
+    <div className="items-center flex justify-center">
       <MotionLink
         href="/"
-        className=" bg-dark text-white font-bold rounded-full px-4 py-3 items-center flex justify-center "
+        className=" items-center flex justify-center h-16 w-20"
         whileHover={{
-            scale: 1.125,
-            transition: { duration: 2 },
+            scale: 1.2,
+            transition: { duration: 1 },
           }}
           whileTap={{ scale: 0.9 }}
       >
-        Hamza_OK
+        <Logo_Dark />
       </MotionLink>
+
+      {/* <motion.a
+          href="#"
+          target={"_blank"}
+          className="mr-2 w-6"
+          whileHover={{ y: -2 }}
+        >
+          <TwitterIcon />
+        </motion.a> */}
     </div>
+
   );
 };
 export default Logo;
